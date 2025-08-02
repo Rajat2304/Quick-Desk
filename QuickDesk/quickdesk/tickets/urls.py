e225b155befea23bb904311bd5a690ce83288f60
Rajat2304 <rajatduhan1549@gmail.com>
2025-08-02 16:42:47 +0530

    Delete QuickDesk/quickdesk directory

--- QuickDesk/quickdesk/tickets/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TicketListCreateView, TicketDetailView
-
-urlpatterns = [
-    path('', TicketListCreateView.as_view(), name='ticket-list-create'),
-    path('<int:pk>/', TicketDetailView.as_view(), name='ticket-detail'),
-]
